d7ff7854ccb4915021b12fd1c4f6032eb033a6a4
Emmanuel <itelemma0@gmail.com>
2026-07-08 02:37:25 +0000

    Initial PizzHub setup with PostgreSQL and Prisma

---
+++ prisma/migrations/20260708015308_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Pizza" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Pizza_pkey" PRIMARY KEY ("id")
+);
